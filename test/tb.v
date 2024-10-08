8e2113dabe65c4a3a7ce58eb90a395294bea37c9
SCATE user <dont_use_this_account@example.com>
2024-10-08 17:12:04 -0400

    update

--- test/tb.v
+++ test/tb.v
@@ -27,10 +27,10 @@ module tb ();
   tt_um_led_cipher user_project (
 
       // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(1'b1),
-      .VGND(1'b0),
-`endif
+//`ifdef GL_TEST
+//      .VPWR(1'b1),
+//      .VGND(1'b0),
+//`endif
 
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
